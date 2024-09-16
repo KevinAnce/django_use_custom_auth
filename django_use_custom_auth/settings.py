b51d27d5cf56221b99774e04e9037222d14993bc
TIAKOUANG DJOU KEVIN <djoukevin1469@gmail.com>
2024-09-16 20:28:59 +0100

    configure .gitignore

--- django_use_custom_auth/settings.py
+++ django_use_custom_auth/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'django_use_custom_auth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
